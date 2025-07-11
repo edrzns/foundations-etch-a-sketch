c723bddcb1ed267a6447f4d550f4ebe1b0d1b8ce
Edijs Rozens <edijs.rozens@gmail.com>
2025-07-11 15:43:09 +0300

    Add persistent hover effect

--- scripts/script.js
+++ scripts/script.js
@@ -5,5 +5,10 @@ const totalSquares = 16 * 16;
 for (let i = 0; i < totalSquares; i++) {
     const square = document.createElement('div');
     square.classList.add('grid-square');
+
+    square.addEventListener('mouseenter', () => {
+        square.classList.add('hovered');
+    });
+
     container.appendChild(square);
 }
